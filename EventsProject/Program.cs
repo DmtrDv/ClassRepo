81ba2efdcb6bc2a654fb6850cfcc8b7925e4dc5a
who you <dmitriybychkov75146@mail.ru>
2025-01-24 21:18:43 +0300

    Добавил удаление пробелов в конце строк

---
+++ EventsProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventsProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Sender sender = new Sender();
+
+            ReceiverConsole receiverConsole = new ReceiverConsole();
+            receiverConsole.Connect(sender);
+
+            ReceiverFile receiverFile = new ReceiverFile();
+            receiverFile.Connect(sender);
+
+            sender.Send();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EventsProject/ReceiverFile.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace EventsProject
+{
+    public class ReceiverFile
+    {
+        public void Connect(Sender s)
+        {
+            s.Notify += SaveMessageToFile;
+        }
+
+        private void SaveMessageToFile(string m)
+        {
+            File.WriteAllText(Path.Combine("D:\\Бойков\\P30", "ReceiverFile.txt"), m);
+        }
+    }
+}
